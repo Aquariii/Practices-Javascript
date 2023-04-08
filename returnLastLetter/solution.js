5f5fe3ca0a2fb7cb2e280410e9543ffa4fb4b6e8
Aquariii <himongayareggie@gmail.com>
2023-04-09 01:37:00 +0800

    add solution.js in returnLastLetter exercise

---
+++ returnLastLetter/solution.js
@@ -0,0 +1,9 @@
+/* Solution in finding the last letter of the string */
+function lastLetter(str){
+    return alert(str.charAt(str.length1)); //Returns the last index of the string//
+}
+lastLetter(window.prompt("Enter a string"));
+
+/* The Arrow function version of the above exercise */
+/*(let lastLetter=(str)=>alert(str.charAt(str.length -1));
+lastLetter(window.prompt("Enter a string"));*/
